168d1a7e56d852b718ff88c2d7c08c63d89cf911
eric Kim <gudals147@naver.com>
2021-03-16 23:12:30 +0900

    20.03.16_#2.21_finished

---
+++ db.js
@@ -0,0 +1,14 @@
+export const videos = [
+  {
+    id: 1235452,
+    title: "Video awesome",
+    description: "This is something I love",
+    views: 24,
+    videoFile: "https://archive.org/details/BigBuckBunny_1234",
+    creator: {
+      id: 777777,
+      name: "Eric-Kim",
+      email: "eric@naver,com",
+    },
+  },
+];
